877ab4ef9402eab1d8f31d0409fcadd2ce85d989
VicenteFlame <vicente.munoz.o@ug.uchile.cl>
2024-09-02 23:10:28 -0400

    casi listo faltan retoques finales

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-"# App-Web" 
+# Readme
+
+
+
+## Características
+
+1. **Agregar Donación**: 
+   - Los usuarios pueden contribuir fácilmente agregando nuevas donaciones de dispositivos.
+   - El formulario de donación recibe información completa, incluyendo:
+     - Datos de contacto del donante (nombre, correo electrónico, número de teléfono)
+     - Detalles del dispositivo (nombre, descripción, tipo, años de uso)
+     - Estado funcional del dispositivo
+     - Opción para subir fotos del dispositivo
+   - Se pueden agregar múltiples dispositivos a una sola donación.
+   - Si no se cumple con el formato deseado en algún dato, el formulario indicará error cuando se intente enviar.
+   - La información aún no es almacenada, ya que esta tarea estaba más dirigida a la interfaz
+
+2. **Ver Dispositivos**: 
+   - Una interfaz amigable muestra todos los dispositivos disponibles para donación.
+   - La lista incluye información clave a simple vista: tipo de dispositivo, nombre, estado funcional, ubicación y una imagen en miniatura.
+   - Los usuarios pueden revisar rápidamente las diversas opciones de donación.
+
+3. **Información del Dispositivo**: 
+   - Cada dispositivo tiene una página dedicada con información detallada.
+   - Incluye detalles completos del donante y especificaciones del dispositivo.
+   - Presenta imágenes de alta calidad del dispositivo para una mejor evaluación.
+   - Por ahora, al hacer click en cualquier fila se abré una imagen generica, más adelante estará conectada con la información de cada dispositivo.
+
+4. **Sistema de Comentarios**: 
+   - Los usuarios pueden dejar comentarios en los listados de dispositivos específicos.
+   - Fomenta la participación de la comunidad y permite a los posibles receptores hacer preguntas.
+   - Muestra el nombre del comentarista y la fecha del comentario para mayor transparencia.
+   - Como indicó el profe en la clase, la plataforma web aún no deberá almacenar ni agrega los comentarios, por lo que se tomó la decisión de no implementar eso aún.
+
+## Estructura del Proyecto
+
+El proyecto está construido utilizando HTML, CSS y JavaScript. Los componentes clave incluyen:
+
+- `index.html`: La página de inicio, con botones de navegación a las funcionalidades principales.
+- `agregar-donacion.html`: Un formulario completo para enviar nuevas donaciones de dispositivos.
+- `ver-dispositivos.html`: Muestra una lista paginada de todos los dispositivos donados disponibles.
+- `informacion-dispositivo.html`: Proporciona información detallada sobre un dispositivo específico y aloja la sección de comentarios.
+- Archivos CSS: Manejan el estilo y el diseño responsivo de la aplicación.
+- Archivos JavaScript : Gestionan validaciones de formularios, comportamientos de los botones, y el menú de selecciones para las regiones y comunas.
+
+
--- js/informacion-dispositivo.js
+++ js/informacion-dispositivo.js
@@ -1,3 +1,119 @@
 document.querySelector('.zoomable-image').addEventListener('click', function() {
     this.classList.toggle('enlarge');
 });
+
+const validateName = (name) => {
+    if(!name) return false;
+    let lengthValid = name.trim().length >= 3 && name.trim().length <= 80 ;
+    return lengthValid;
+  }
+const validateComentario = (comentario) => {
+    if(!comentario) return false;
+    let lengthValid = comentario.trim().length >= 5;
+    return lengthValid;
+  }
+  
+
+
+const validateForm = () => {
+    // obtener elementos del DOM usando el nombre del formulario.
+    let myForm = document.forms["conf-form"];
+    let name = myForm["nombre"].value;
+    let comentario = myForm["comentario"].value;
+
+  
+    // variables auxiliares de validación y función.
+    let invalidInputs = [];
+    let isValid = true;
+    const setInvalidInput = (inputName) => {
+      invalidInputs.push(inputName);
+      isValid &&= false;
+    };
+  
+    // lógica de validación
+    if (!validateName(name)) {
+      setInvalidInput("Nombre");
+    }
+    if (!validateComentario(comentario)) {
+      setInvalidInput("Comentario");
+    }
+
+  
+    // finalmente mostrar la validación
+    let validationBox = document.getElementById("val-box");
+    let validationMessageElem = document.getElementById("val-msg");
+    let validationListElem = document.getElementById("val-list");
+    let formContainer = document.querySelector(".main-container");
+  
+    if (!isValid) {
+      validationListElem.textContent = "";
+      // agregar elementos inválidos al elemento val-list.
+      for (input of invalidInputs) {
+        let listElement = document.createElement("li");
+        listElement.innerText = input;
+        validationListElem.append(listElement);
+      }
+      // establecer val-msg
+      validationMessageElem.innerText = "Los siguientes campos son inválidos:";
+  
+      // aplicar estilos de error
+      validationBox.style.backgroundColor = "#ffdddd";
+      validationBox.style.borderLeftColor = "#f44336";
+  
+      // hacer visible el mensaje de validación
+      validationBox.hidden = false;
+    } else {
+        
+        
+
+  
+      // establecer mensaje de éxito
+      validationMessageElem.innerText = "Se agregó el comentario ";
+      validationListElem.textContent = "";
+  
+      // aplicar estilos de éxito
+      validationBox.style.backgroundColor = "#ddffdd";
+      validationBox.style.borderLeftColor = "#4CAF50";
+  
+
+      // hacer visible el mensaje de validación
+      validationBox.hidden = false;
+
+      // contenedor confesion
+      const confContainer = document.createElement("div");
+      confContainer.className = "conf-container";
+
+
+      // username
+      const userNameParagraph = document.createElement("p");
+      userNameParagraph.innerText = localStorage.getItem("nombre");
+
+      // user container
+      const confAuthor = document.createElement("div");
+      confAuthor.className = "conf-author";
+      confAuthor.appendChild(userImage);     confAuthor.appendChild(userNameParagraph);
+
+      // texto confesion
+      const confesion = document.createElement("p");
+      confesion.innerText = localStorage.getItem("conf-text-area");;
+
+      // agregamos los elementos al cont. de la confesion
+      confContainer.appendChild(confAuthor);
+      confContainer.appendChild(confesion);
+
+      // agregamos la confesion a la lista
+      let confList = document.getElementById("conf-list");
+      confList.appendChild(confContainer);
+    }
+  };
+  
+  
+  let submitBtn = document.getElementById("submit-conf-btn");
+  submitBtn.addEventListener("click", validateForm);
+  
+  let homeBtn = document.getElementById("home-btn");
+  homeBtn.addEventListener('click', function() {
+    window.location.href = 'index.html'; 
+});
+  
+  
